59d6ec4a339835231b5f0659e7a29cb982fcb203
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-19 11:15:56 -0400

    feat(canvas): implement canvas cell color change on hover
    This commit introduces a new feature that allows users to change the
    color of the canvas cells by hovering over them.
    This enhances the user experience by providing visual feedback when
    interacting with the canvas.
    
    The changes include:
    - Adding an event listener to each canvas cell in script.js to add the
    'game__canvas-cell_change-color' class on mouseover.
    - Adding the 'game__canvas-cell_change-color' class in style.css to set
    the background color to black.

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ for (let index = 0; index < 16 * 16; index++) {
   canvasCell.classList.add("game__canvas-cell");
   canvasCell.textContent = `${index}`;
 
+  canvasCell.addEventListener("mouseover", function () {
+    this.classList.add("game__canvas-cell_change-color");
+  });
+
   canvas.appendChild(canvasCell);
 }
