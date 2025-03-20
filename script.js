fccdc060ccac507756e186b3b517fafff8afb4e5
d2038-dev <198118887+d2038-dev@users.noreply.github.com>
2025-03-20 12:19:45 -0400

    feat(game): implement grid size and color controls
    This commit introduces new features to control the etch-a-sketch game:
    
    - Added "New grid" button to allow users to change the grid size.
    - Added "Black color" button to enable drawing in black color.
    - Added "Random color" button to enable drawing in random colors.
    
    These changes enhance the user experience by providing more
    customization options for the game.

--- script.js
+++ script.js
@@ -1,13 +1,78 @@
+const gridSizeBTN = document.querySelector("[data-id='grid-size-btn']");
+const blackColorBTN = document.querySelector("[data-id='black-color-btn']");
+const randomColorBTN = document.querySelector("[data-id='random-color-btn']");
 const canvas = document.querySelector("[data-id='canvas']");
 
-for (let index = 0; index < 16 * 16; index++) {
-  const canvasCell = document.createElement("div");
-  canvasCell.classList.add("game__canvas-cell");
-  canvasCell.textContent = `${index}`;
+renderGrid(16);
 
-  canvasCell.addEventListener("mouseover", function () {
-    this.classList.add("game__canvas-cell_change-color");
+function initializeGrid() {
+  const gridSize = getGridSize();
+  clearCanvas();
+  renderGrid(gridSize);
+}
+
+function enableBlackColorMode() {
+  canvas.childNodes.forEach((canvasCell) => {
+    canvasCell.removeEventListener("mouseover", makeBackgroundRandom);
+    canvasCell.addEventListener("mouseover", makeBackgroundBlack);
   });
+}
 
-  canvas.appendChild(canvasCell);
+function enableRandomColorMode() {
+  canvas.childNodes.forEach((canvasCell) => {
+    canvasCell.removeEventListener("mouseover", makeBackgroundBlack);
+    canvasCell.addEventListener("mouseover", makeBackgroundRandom);
+  });
+}
+
+gridSizeBTN.addEventListener("click", initializeGrid);
+blackColorBTN.addEventListener("click", enableBlackColorMode);
+randomColorBTN.addEventListener("click", enableRandomColorMode);
+
+function getGridSize() {
+  let gridSize;
+  do {
+    const userResponse = prompt("Enter size (max. 100)", 16);
+    gridSize = +userResponse;
+  } while (!(gridSize <= 100 && gridSize > 0));
+
+  return gridSize;
+}
+
+function clearCanvas() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+}
+
+function renderGrid(gridSize) {
+  const cellSize = canvas.clientWidth / gridSize;
+  for (let index = 0; index < gridSize * gridSize; index++) {
+    const canvasCell = document.createElement("div");
+    canvasCell.style.cssText = `width: ${cellSize}px; height: ${cellSize}px;`;
+
+    canvasCell.addEventListener("mouseover", makeBackgroundBlack);
+
+    canvas.appendChild(canvasCell);
+  }
+}
+
+function makeBackgroundBlack() {
+  this.style.backgroundColor = "black";
+}
+
+function makeBackgroundRandom() {
+  this.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  const red = getRandomRGBNumber();
+  const green = getRandomRGBNumber();
+  const blue = getRandomRGBNumber();
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function getRandomRGBNumber() {
+  return Math.floor(Math.random() * 256);
 }
